5b9f0aea27ab79b41d06971dc245c45beac3413a
Zbigniew Stefaniuk <zbigniewmarekstefaniuk@gmail.com>
2021-07-16 22:50:04 +0200

    feat: navbar

--- src/app/components/global/navbar/Navbar.tsx
+++ src/app/components/global/navbar/Navbar.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from '../../../styles/navbar/navbar.styles.module.css';
+
+import NavbarData from './navbarData';
 
 const Navbar = () => {
+  const renderNavbarItems = ({ title, url }: { title: string; url: string }): JSX.Element => {
+    return (
+      <li key={title}>
+        <Link className={styles.navLink} to={url}>
+          {title}
+        </Link>
+      </li>
+    );
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/post">Users</Link>
-        </li>
-      </ul>
+    <nav className={styles.navbar}>
+      <div>
+        <ul className={styles.navMenu}>{Object.values(NavbarData).map(renderNavbarItems)}</ul>
+      </div>
     </nav>
   );
 };
---
+++ src/app/components/global/navbar/navbarData.ts
@@ -0,0 +1,18 @@
+import ROUTES from '../../../utils/routes';
+
+const NavbarData = {
+  home: {
+    title: 'Home',
+    url: ROUTES.home,
+  },
+  about: {
+    title: 'About',
+    url: ROUTES.about,
+  },
+  post: {
+    title: 'Post',
+    url: ROUTES.post,
+  },
+};
+
+export default NavbarData;
